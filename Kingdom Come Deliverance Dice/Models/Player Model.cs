4189d3fb5b11f4bfb34a84e72d80b5c5f4548c5e
Toby Hunter <tobyhunter1999@btopenworld.com>
2024-10-30 11:35:04 +0000

    Models
    Created and altered the models to make them more object oriented and workable.

--- Kingdom Come Deliverance Dice/Models/Player Model.cs
+++ Kingdom Come Deliverance Dice/Models/Player Model.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright © - unpublished - Toby Hunter
 namespace Kingdom_Come_Deliverance_Dice.Models
 {
-    internal class DiceModel
+    internal class PlayerModel
     {
         public int Number { get; set; }
-        public int Roll { get; set; }
-        public bool Selected { get; set; }
+        public int Pot { get; set; }
+        public List<DiceModel> Dice { get; set; } = new();
     }
 }
