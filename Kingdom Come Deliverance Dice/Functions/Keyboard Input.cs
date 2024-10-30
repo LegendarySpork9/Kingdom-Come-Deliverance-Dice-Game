c2f9d0b4c8d165294a0fee2006a613382061d555
Toby Hunter <tobyhunter1999@btopenworld.com>
2024-10-30 11:49:48 +0000

    Keyboard Input
    Created the function to the capture the inputs from the keyboard. Will fill and adapt as the main code is completed.

---
+++ Kingdom Come Deliverance Dice/Functions/Keyboard Input.cs
@@ -0,0 +1,30 @@
+﻿// Copyright © - unpublished - Toby Hunter
+namespace Kingdom_Come_Deliverance_Dice
+{
+    internal class KeyboardInput
+    {
+        public void MenuInput(ConsoleKey Key)
+        {
+            switch (Key)
+            {
+                
+            }
+        }
+
+        public void SettingsInput(ConsoleKey Key)
+        {
+            switch (Key)
+            {
+                
+            }
+        }
+
+        public void GameInput(ConsoleKey Key)
+        {
+            switch (Key)
+            {
+                
+            }
+        }
+    }
+}
